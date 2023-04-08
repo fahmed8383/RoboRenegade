4cde55250546424a37566f1b0cde493fe101acd0
Kevin Kannammalil <kevinrk2000@gmail.com>
2023-04-08 15:35:55 -0400

    balanced laser dmg

--- Robo Renegade/Assets/EnemyAssets/enemyScript.cs
+++ Robo Renegade/Assets/EnemyAssets/enemyScript.cs
@@ -80,7 +80,7 @@ public class enemyScript : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("LaserBeam")){
             // Debug.Log("LaserBeam Collision");
-            TakeDamage(20);
+            TakeDamage(15);
         }
     }
 
